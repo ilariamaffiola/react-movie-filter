f528f31239eba89bb9037800b748b4f31d9958c3
Ilaria <maffiolailaria1@gmail.com>
2025-05-12 16:40:24 +0200

    [UPD]Improve code comments for clarity and maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,17 @@ function App() {
     { title: 'Interstellar', genre: 'Fantascienza' },
     { title: 'Pulp Fiction', genre: 'Thriller' },
   ];
-  const [genre, setGenre] = useState("");
-  const [filteredFilms, setFilteredFilms] = useState(films);
+  const [genre, setGenre] = useState(""); //genere selezionato
+  const [filteredFilms, setFilteredFilms] = useState(films); //film da visualizzare
   const handleChange = (e) => {
-    setGenre(e.target.value)
+    setGenre(e.target.value) //quando un genere viene selezionato viene salvato in genere
   }
+  //useEffect si attiva ogni volta che un genere viene selezionato un genere
   useEffect(() => {
-    if(genre === "") {
+    if(genre === "") { //se genere è vuoto mostra tutti i film
       setFilteredFilms(films);
     }
-    else{
+    else{ //altrimenti mostra i film che corrispondono al genere scelto
       const filtered = films.filter((film) => film.genre === genre);
       setFilteredFilms(filtered);
     }
@@ -30,6 +31,7 @@ function App() {
         <div className='row justify-content-center'>
           <div className='col-8 d-flex align-items-center'>
             <form className="text-center">
+              {/*è un select controllato, che mostra il valore attuale e lo aggiorna al cambio attraverso lo stato*/}
               <select value={genre} onChange={handleChange} name="films" id="films">
                  <option value="">Scegli un genere</option>
                 <option value="Fantascienza">Fantascienza</option>
@@ -40,6 +42,7 @@ function App() {
             </form>
             <div className="list-group">
               <ul>
+                {/* bisogna mettere filteredFilms oerché films è l'array completo non filtrato */}
                   {filteredFilms.map((film, index) =>(
                     <li
                      className="list-group-item d-flex justify-content-center"
