4df214b49782f83ce8bc194fc434640de501961d
Ilaria <maffiolailaria1@gmail.com>
2025-05-12 15:47:45 +0200

    Add Bootstrap styling and film list to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import { useState } from 'react'
 
 
 function App() {
+  let films= [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+  ];
 
   return (
     <>
-      
+      <div className="container">
+        <div className='row'>
+          <div className='col-6'>
+            <div className="list-group">
+              <ul>
+                  {films.map((film, index) =>{
+                    <li
+                     className="list-group item"
+                     key={index}
+                    >
+                      {film.title}
+                    </li>
+                  })}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
