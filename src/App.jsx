4b401d5d59bb8e11f5566cf5d221d58f6e901387
Ilaria <maffiolailaria1@gmail.com>
2025-05-12 16:23:39 +0200

    [UPD]Implement genre filtering for film list and enhance layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import { useState, useEffect } from "react";
 
 function App() {
   let films= [
@@ -9,17 +9,40 @@ function App() {
     { title: 'Interstellar', genre: 'Fantascienza' },
     { title: 'Pulp Fiction', genre: 'Thriller' },
   ];
+  const [genre, setGenre] = useState("");
+  const [filteredFilms, setFilteredFilms] = useState(films);
+  const handleChange = (e) => {
+    setGenre(e.target.value)
+  }
+  useEffect(() => {
+    if(genre === "") {
+      setFilteredFilms(films);
+    }
+    else{
+      const filtered = films.filter((film) => film.genre === genre);
+      setFilteredFilms(filtered);
+    }
+  }, [genre]);
 
   return (
     <>
       <div className="container">
-        <div className='row'>
-          <div className='col-6'>
+        <div className='row justify-content-center'>
+          <div className='col-8 d-flex align-items-center'>
+            <form className="text-center">
+              <select value={genre} onChange={handleChange} name="films" id="films">
+                 <option value="">Scegli un genere</option>
+                <option value="Fantascienza">Fantascienza</option>
+                <option value="Thriller">Thriller</option>
+                <option value="Romantico">Romantico</option>
+                <option value="Azione">Azione</option>  
+              </select>
+            </form>
             <div className="list-group">
               <ul>
-                  {films.map((film, index) =>(
+                  {filteredFilms.map((film, index) =>(
                     <li
-                     className="list-group-item d-flex justify-content-between align-items-center"
+                     className="list-group-item d-flex justify-content-center"
                      key={index}
                     >
                       {film.title}
